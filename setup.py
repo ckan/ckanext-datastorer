df128133c31972cef0b4c5edef31c186d1c8649b
kindly <kindly@gmail.com>
2012-05-18 15:24:13 +0100

    fix celery dependancy 2.4.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
 	zip_safe=False,
     install_requires = [
         'messytables>=0.2',
-        'celery>=2.4.2',
+        'celery==2.4.2',
         'kombu==2.1.3',
         'kombu-sqlalchemy==1.1.0',
         'ckanext-archiver>=0.0',
