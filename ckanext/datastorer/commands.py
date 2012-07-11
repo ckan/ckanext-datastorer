aafa78f3dc26fcae494a75056e3bb06f9c3b5cfb
Ross Jones <rossdjones@gmail.com>
2012-07-11 11:29:28 +0100

    PEP8

--- ckanext/datastorer/commands.py
+++ ckanext/datastorer/commands.py
@@ -10,21 +10,22 @@ from ckan.model.types import make_uuid
 import logging
 logger = logging.getLogger()
 
+
 class Webstorer(CkanCommand):
     """
     Upload all available resources to the webstore
 
     Usage:
 
-        paster datastorer update 
-           - Archive all resources or just those belonging to a specific package 
-             if a package id is provided
+        paster datastorer update
+           - Archive all resources or just those belonging to a specific
+             package if a package id is provided
 
-    """    
+    """
     summary = __doc__.split('\n')[0]
     usage = __doc__
-    min_args = 1 
-    max_args = 1 
+    min_args = 1
+    max_args = 1
 
     def command(self):
         """
@@ -39,7 +40,8 @@ class Webstorer(CkanCommand):
         #import after load config so CKAN_CONFIG evironment variable can be set
         from ckan.lib.celery_app import celery
         import tasks
-        user = get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
+        user = get_action('get_site_user')({'model': model,
+                                            'ignore_auth': True}, {})
         context = json.dumps({
             'site_url': config['ckan.site_url'],
             'apikey': user.get('apikey'),
@@ -49,22 +51,26 @@ class Webstorer(CkanCommand):
         api_url = urlparse.urljoin(config['ckan.site_url'], 'api/action')
 
         if cmd == 'update':
-            response = requests.post(api_url + '/current_package_list_with_resources', "{}")
+            response = requests.post(api_url +
+                                     '/current_package_list_with_resources',
+                                     "{}")
             packages = json.loads(response.content).get('result')
 
             for package in packages:
                 for resource in package.get('resources', []):
                     data = json.dumps(resource, {'model': model})
-                    
+
                     if resource['webstore_url']:
                         continue
                     mimetype = resource['mimetype']
-                    if mimetype and (mimetype not in tasks.DATA_FORMATS 
-                                     or resource['format'] not in tasks.DATA_FORMATS):
+                    if mimetype and (mimetype not in tasks.DATA_FORMATS
+                                     or resource['format'] not in
+                                     tasks.DATA_FORMATS):
                         continue
 
-                    logger.info('Webstoring resource from resource %s from package %s' % (
-                        resource['url'], package['name']))
+                    logger.info('Webstoring resource from resource %s from '
+                                'package %s' % (resource['url'],
+                                                package['name']))
 
                     task_id = make_uuid()
                     datastorer_task_status = {
@@ -76,15 +82,14 @@ class Webstorer(CkanCommand):
                         'last_updated': datetime.now().isoformat()
                     }
                     datastorer_task_context = {
-                        'model': model, 
+                        'model': model,
                         'user': user.get('name')
                     }
 
-                    get_action('task_status_update')(datastorer_task_context, 
+                    get_action('task_status_update')(datastorer_task_context,
                                                      datastorer_task_status)
-                    celery.send_task("datastorer.upload", 
-                                     args=[context, data], 
+                    celery.send_task("datastorer.upload",
+                                     args=[context, data],
                                      task_id=task_id)
         else:
             logger.error('Command %s not recognized' % (cmd,))
-
