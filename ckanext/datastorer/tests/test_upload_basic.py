420b27cdf4008773de91f544da3742171501cc12
Dominik Moritz <domoritz@gmail.com>
2012-10-10 16:03:08 +0100

    test larger files (4000 rows)

--- ckanext/datastorer/tests/test_upload_basic.py
+++ ckanext/datastorer/tests/test_upload_basic.py
@@ -268,6 +268,31 @@ class TestUploadBasic(object):
                                               {u'type': u'text', u'id': u'Cost Centre Description'},
                                               {u'type': u'text', u'id': u'Grand Total'}], result['result']['fields']
 
+    def test_long_file(self):
+
+        data = {
+            'url': 'http://0.0.0.0:50001/static/long.csv',
+            'format': 'csv'}
+
+        context = {'site_url': 'http://%s' % self.host,
+                   'apikey': self.api_key,
+                   'site_user_apikey': self.api_key,
+                   }
+
+        resource_id = self.make_resource_id()
+        data['id'] = resource_id
+
+        tasks.datastorer_upload(json.dumps(context), json.dumps(data))
+
+        response = requests.get(
+            'http://%s/api/action/datastore_search?resource_id=%s' % (self.host, resource_id),
+             headers={"content-type": "application/json"})
+
+        result = json.loads(response.content)
+
+        assert result['result']['total'] == 4000, (result['result']['total'], resource_id)
+        assert len(result['result']['records']) == 100
+
     def test_error_bad_url(self):
 
         data = {'url': 'http://0.0.0.0:50001/static/3ffdcd',
