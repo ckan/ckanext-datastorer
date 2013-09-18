122f217247cdeb25ed10d3e1d76c420ad883c551
joetsoi <joe.yeung.tsoi@gmail.com>
2013-09-18 11:53:11 +0100

    gracefully handle exception when field size is too large

--- ckanext/datastorer/commands.py
+++ ckanext/datastorer/commands.py
@@ -301,7 +301,15 @@ class AddToDataStore(CkanCommand):
 
         # only first sheet in xls for time being
         row_set = table_sets.tables[0]
-        offset, headers = headers_guess(row_set.sample)
+        try:
+            offset, headers = headers_guess(row_set.sample)
+        except messytables.error.ReadError as e:
+            logger.exception(e)
+            return {'success': False,
+                    'resource': resource['id'],
+                    'error': 'Error parsing the resource'
+            }
+
         row_set.register_processor(headers_processor(headers))
         row_set.register_processor(offset_processor(offset + 1))
         row_set.register_processor(datetime_procesor())
